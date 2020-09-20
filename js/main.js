afc07d272ee9e8a143152f5f9b22ccf5b0ab16bf
guillaumequillet <bgachats@yahoo.fr>
2020-09-20 08:16:31 +0200

    implemented drawing with rotation

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@ class Game extends YumeWindow {
     loadContent() {
         this.sprite = new YumeImage(this.ctx, "../images/secretary.png")
         this.x = 0
+        this.angle = 0
     }
 
     update() {
-        this.x += 0.5
+        // this.x += 0.5
+        this.angle += 1
     }
 
     draw() {
         this.drawRect(10, 10, 200, 20, "purple")
-        this.sprite.draw(Math.floor(this.x), 0)
+        this.sprite.drawRot(160, 120, this.angle, 0.5, 1)
     }
 }
 
--- js/yume/YumeImage.js
+++ js/yume/YumeImage.js
@@ -22,4 +22,14 @@ class YumeImage {
             this.ctx.drawImage(this.image, x, y)
         }
     }
+
+    drawRot(x, y, angle, pivotX = 0.5, pivotY = 0.5) {
+        this.ctx.save()
+        this.ctx.translate(x, y);
+        this.ctx.rotate(angle * Math.PI / 180);
+        this.ctx.drawImage(this.image, -this.image.width * pivotX, -this.image.height * pivotY, 
+            this.image.width, this.image.height
+        );
+        this.ctx.restore()
+    }
 }
